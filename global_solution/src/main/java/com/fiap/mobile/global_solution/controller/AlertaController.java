b9ff6da972907c4ef104f2f4d620d9ecfb59a9c1
Lacerdex <gabriel.gamexx1@hotmail.com>
2026-05-29 15:05:46 -0300

    feat: criação do controller Alerta

---
+++ global_solution/src/main/java/com/fiap/mobile/global_solution/controller/AlertaController.java
@@ -0,0 +1,52 @@
+package com.fiap.mobile.global_solution.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fiap.mobile.global_solution.model.Alerta;
+import com.fiap.mobile.global_solution.service.AlertaService;
+
+@RestController
+@RequestMapping("/api/alertas")
+@CrossOrigin(origins = "*") // Permite que o app mobile acesse sem erros de CORS
+public class AlertaController {
+
+    @Autowired
+    private AlertaService alertaService;
+
+    @PostMapping
+    public ResponseEntity<Alerta> criarAlerta(@RequestBody Alerta alerta) {
+        Alerta novoAlerta = alertaService.salvarAlerta(alerta);
+        return new ResponseEntity<>(novoAlerta, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Alerta>> listarAlertas() {
+        return ResponseEntity.ok(alertaService.listarTodos());
+    }
+
+    @GetMapping("/filtro")
+    public ResponseEntity<List<Alerta>> filtrarPorGravidade(@RequestParam String gravidade) {
+        return ResponseEntity.ok(alertaService.listarPorGravidade(gravidade));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Alerta> buscarPorId(@PathVariable Long id) {
+        Alerta alerta = alertaService.buscarPorId(id);
+        if (alerta == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(alerta);
+    }
+}
