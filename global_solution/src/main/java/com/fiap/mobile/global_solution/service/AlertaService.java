491222f3070fc94d0f7fd8adea6f54e518dfc29b
Lacerdex <gabriel.gamexx1@hotmail.com>
2026-05-29 15:05:07 -0300

    feat: criação da camada service de Alerta

---
+++ global_solution/src/main/java/com/fiap/mobile/global_solution/service/AlertaService.java
@@ -0,0 +1,34 @@
+package com.fiap.mobile.global_solution.service;
+
+import com.fiap.mobile.global_solution.model.Alerta;
+import com.fiap.mobile.global_solution.repository.AlertaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class AlertaService {
+
+    @Autowired
+    private AlertaRepository alertaRepository;
+
+    public Alerta salvarAlerta(Alerta alerta) {
+        // Regra de Negócio Básica
+        if (alerta.getGravidade() == null) {
+            alerta.setGravidade("INFO");
+        }
+        return alertaRepository.save(alerta.toUpperCase());
+    }
+
+    public List<Alerta> listarTodos() {
+        return alertaRepository.findAll();
+    }
+
+    public Alerta buscarPorId(Long id) {
+        return alertaRepository.findById(id).orElse(null);
+    }
+
+    public List<Alerta> listarPorGravidade(String gravidade) {
+        return alertaRepository.findByGravidade(gravidade.toUpperCase());
+    }
+}
