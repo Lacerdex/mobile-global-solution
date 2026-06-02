aa8530177261ac69d6260fd5efcf7ac0d8b4a57e
Lacerdex <gabriel.gamexx1@hotmail.com>
2026-06-02 17:31:48 -0300

    chore: adicionando index para facil exportação de cada modulo

---
+++ interface_gs/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as SensorCard } from './SensorCard';
+export { default as AlertaCard } from './AlertaCard';
+export { default as EventoCard } from './EventoCard';
+export { default as NovoRegistroModal } from './NovoRegistroModal';
---
+++ interface_gs/src/types/index.ts
@@ -0,0 +1,3 @@
+export { default as Sensor } from './Sensor';
+export { default as Alerta } from './Alerta';
+export { default as Evento } from './Evento';
